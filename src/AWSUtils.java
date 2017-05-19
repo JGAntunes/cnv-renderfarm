0503d1804936da2313d3bc4308328b4e509beb86
JGAntunes <j.goncalo.antunes@gmail.com>
2017-05-19 03:25:18 +0100

    add dynamodb utility methods

--- src/AWSUtils.java
+++ src/AWSUtils.java
@@ -4,17 +4,26 @@ import com.amazonaws.services.autoscaling.*;
 import com.amazonaws.services.autoscaling.model.*;
 import com.amazonaws.services.ec2.*;
 import com.amazonaws.services.ec2.model.*;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.amazonaws.services.dynamodbv2.document.*;
 
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 public class AWSUtils {
   
   public static final String AUTOSCALING_NAME = "cnv";
+  public static final String REQUEST_TABLE_NAME = "request";
+
   public static final AmazonAutoScaling autoscalingClient = new AmazonAutoScalingClient();
   public static final AmazonEC2 ec2Client = AmazonEC2ClientBuilder.defaultClient();
+  public static final AmazonDynamoDB dynamoClient = AmazonDynamoDBClientBuilder.defaultClient();
+
+  public static Table requestTable;
 
   private AWSUtils () {}
 
@@ -55,7 +64,74 @@ public class AWSUtils {
     return publicIps;
   }
 
+  public static void setupDynamoTables() {
+    DynamoDB dynamo = new DynamoDB(dynamoClient);
+    try {
+      TableDescription table = dynamoClient.describeTable(new DescribeTableRequest(REQUEST_TABLE_NAME))
+        .getTable();
+      if (!TableStatus.ACTIVE.toString().equals(table.getTableStatus())) {
+        throw new ResourceNotFoundException(REQUEST_TABLE_NAME);
+      }
+      requestTable = dynamo.getTable(REQUEST_TABLE_NAME);
+    } catch (ResourceNotFoundException rnfe) {
+      // This means the table doesn't exist in the account yet
+      List<AttributeDefinition> attributeDefinitions= new ArrayList<AttributeDefinition>();
+      attributeDefinitions.add(new AttributeDefinition().withAttributeName("File").withAttributeType("S"));
+      attributeDefinitions.add(new AttributeDefinition().withAttributeName("Size").withAttributeType("N"));
+
+      List<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
+      keySchema.add(new KeySchemaElement().withAttributeName("File").withKeyType(KeyType.HASH));
+      keySchema.add(new KeySchemaElement().withAttributeName("Size").withKeyType(KeyType.RANGE));
+      CreateTableRequest request = new CreateTableRequest()
+        .withTableName(REQUEST_TABLE_NAME)
+        .withKeySchema(keySchema)
+        .withAttributeDefinitions(attributeDefinitions)
+        .withProvisionedThroughput(new ProvisionedThroughput()
+            .withReadCapacityUnits(1L)
+            .withWriteCapacityUnits(1L));
+
+      requestTable = dynamo.createTable(request);
+      try {  
+        requestTable.waitForActive();
+      } catch (Exception e) {
+        System.err.println("Error handling table creation " + e.getMessage());
+      }
+      System.out.println("Done");
+    }   
+  }
+
+  public static void addItemToRequest(String fileName, int size, int value) {
+    if (requestTable == null) {
+      throw new UnsupportedOperationException("Request table not setup - run setupDynamoTables first");
+    }
+
+    AttributeUpdate update = new AttributeUpdate("value").put(value);
+    try {
+      UpdateItemOutcome outcome = requestTable.updateItem("File", fileName,  "Size", size, update);
+      System.out.println("UpdateItem succeeded:\n");
+    } catch (Exception e) {
+      System.err.println("Unable to add item to table " + e.getMessage());
+    }
+  }
+
+  public static void getWorstCaseRequest(String fileName, int size) {
+    if (requestTable == null) {
+      throw new UnsupportedOperationException("Request table not setup - run setupDynamoTables first");
+    }
+    
+    ItemCollection<QueryOutcome> items = requestTable.query("File", fileName, new RangeKeyCondition("Size").ge(size));
+    Iterator<Item> iter = items.iterator(); 
+      System.out.println(iter.next().get("value"));
+  }
+
   public static void main(String[] args) {
     System.out.println(getAvailableInstances().size());
+    setupDynamoTables();
+    try{
+      addItemToRequest("test01.txt", 1500, 6069);
+    } catch(Exception e) {
+      System.out.println(e.getMessage());
+    }
+    getWorstCaseRequest("test01.txt", 1000);
   }
 }
