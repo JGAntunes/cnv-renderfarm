a7e3f58cfa6ef31560161b179df7a85e8cc430df
JGAntunes <j.goncalo.antunes@gmail.com>
2017-07-20 21:14:46 +0100

    No need for tty session on docker build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 docker build --rm --tag cnv .
 
 if [ "$RUN" != "" ]; then
-  docker run -v ~/.aws:/root/.aws --rm -p 8000:80 -it cnv
+  docker run -v ~/.aws:/root/.aws --rm -p 8000:80 cnv
 fi;
 
 set +ex
