563d97683f8199d553f39e0ac07b2e32aaf18557
JGAntunes <j.goncalo.antunes@gmail.com>
2017-07-26 22:21:05 +0100

    yolo

--- src/EC2Instance.java
+++ src/EC2Instance.java
@@ -19,14 +19,10 @@ public class EC2Instance {
   private String publicDnsName;
   private String type;
   private Date launchTime;
-  private Thread pollingThread;
-  private boolean isHealthy;
-  private boolean isAvailable;
   private ConcurrentLinkedQueue<RayTracerRequest> inFlightRequests;
 
   public EC2Instance(com.amazonaws.services.ec2.model.Instance ec2Instance) {
     this.inFlightRequests = new ConcurrentLinkedQueue();
-    this.isHealthy = true;
     this.id = ec2Instance.getInstanceId();
     this.publicDnsName = ec2Instance.getPublicDnsName();
     this.type = ec2Instance.getInstanceType();
@@ -50,33 +46,8 @@ public class EC2Instance {
     return launchTime;
   }
 
-  public boolean isHealthy() {
-    return isHealthy;
-  }
-
-  public void setAvailable(boolean availability) {
-    this.isAvailable = availability;
-  }
-
-  public boolean isAvailable() {
-    return isAvailable;
-  }
-
-  public AbstractQueue getInFlightRequests() {
+  public Collection<RayTracerRequest> getInFlightRequests() {
     return inFlightRequests;
-  }
-
-  // Start/stop the poller
-  public void monitor(boolean start) {
-    if (start) {
-      if (this.pollingThread == null || !this.pollingThread.isAlive()) {
-        startPoller();
-      }
-    } else {
-      if (this.pollingThread != null && this.pollingThread.isAlive()) {
-        stopPoller();
-      }
-    }
   }
 
   public RayTracerResponse processRequest(RayTracerRequest request) throws IOException {
@@ -103,42 +74,7 @@ public class EC2Instance {
     return response;
   }
 
-  private void setHealthStatus(boolean status) {
-    String healthStatus = status ? "Healthy" : "Unhealthy";
-    if (this.isHealthy == status) {
-      return;
-    }
-    this.isHealthy = status;
-    // Ensure proper clean up of the poller
-    if (!status) {
-      stopPoller();
-    } else {
-      startPoller();
-    }
-    AWSUtils.setHealthStatus(this.id, healthStatus);
-  }
-
-  private void startPoller () {
-    Poller poller = new Poller();
-    if (this.pollingThread != null) {
-      stopPoller();
-    }
-    this.pollingThread = new Thread(poller);
-    this.pollingThread.start();
-  }
-
-  private void stopPoller () {
-    logger.debug("EC2 monitor stopping");
-    this.pollingThread.interrupt();
-    // Wait 200 milis to for the thread to die, else keep on moving
-    try {
-      this.pollingThread.join(200);
-    } catch (InterruptedException e) {
-      return;
-    }
-  }
-
-  private void runHealthCheck () {
+  private boolean runHealthCheck () {
     String url = "http://" + this.publicDnsName + WebUtils.HEALTHCHECK_PATH;
     int currentRetries = 0;
     for(int i = 0; i < MAX_RETRIES; i++) {
@@ -146,11 +82,11 @@ public class EC2Instance {
         HttpURLConnection conn = WebUtils.request("GET", url, 1000);
         int responseCode = conn.getResponseCode();
         if (responseCode == 200) {
-          setHealthStatus(true);
+          return true;
         } else {
           logger.warning("Healthcheck returned " + responseCode);
           logger.warning("Instance flagged as unhealthy");
-          setHealthStatus(false);
+          return false;
         }
         return;
       } catch (Exception e) {
@@ -159,36 +95,11 @@ public class EC2Instance {
     }
     // Nothing to do but to set the instance as unealthy
     logger.warning("Instance flagged as unhealthy");
-    setHealthStatus(false);
+    return false;
   }
 
   @Override
   public String toString() {
     return this.id + " " + this.publicDnsName;
-  }
-
-  // Clean up thread on GC swipe
-  @Override
-  protected void finalize() throws Throwable {
-    stopPoller();
-  };
-
-  public class Poller implements Runnable {
-    private static final int SLEEP_TIME = 10000;
-
-    @Override
-    public void run() {
-      logger.debug("EC2 monitor starting");
-      boolean run = true;
-      while(run) {
-        try{
-          logger.debug("EC2 monitor running");
-          runHealthCheck();
-          Thread.sleep(SLEEP_TIME);
-        } catch (InterruptedException e) {
-          run = false;
-        }
-      }
-    }
   }
 }
--- src/EC2Pool.java
+++ src/EC2Pool.java
@@ -1,7 +1,80 @@
 package renderfarm;
 
 public class EC2Pool {
-  private List<EC2Instance> instancePool = new List<EC2Instance>();
 
-  public EC2Pool() {}
+  private static final Logger classLogger = new Logger("ec2-pool");
+
+  // Active instance pool
+  private ConcurrentHashMap<String, EC2Instance> healthyPool;
+  // Pool of instances whose health checks actually failed
+  private ConcurrentHashMap<String, EC2Instance> unhealthyPool;
+  // Pool of instances flagged by the scheduler as unable to reply, need to be checked
+  private ConcurrentHashMap<String, EC2Instance> flaggedPool;
+  // Pool of instances marked for termination
+  private ConcurrentHashMap<String, EC2Instance> terminatingPool;
+  private ExecutorService threadPool;
+
+  public EC2Pool() {
+    // TODO Get initial status for instances
+    this.healthyPool = new ConcurrentHashMap();
+    this.unhealthyPool = new ConcurrentHashMap();
+    this.terminatingPool = new ConcurrentHashMap();
+    this.threadPool = Executor.newCachedThreadPool();
+  }
+
+  public Collection<EC2Instance> getAvailableInstances() {
+    return healthyPool.values();
+  }
+
+  private class EC2Poller implements Runnable {
+    private static final int SLEEP_TIME = 10000;
+
+    private EC2Instance instance;
+
+    public EC2Poller (EC2Instance instance) {
+      this.instance = instance;
+    }
+
+    private void healthCheck(boolean reportedStatus) {
+      boolean isHealthy = this.instance.runHealthCheck();
+      if (reportedStatus == null || isHealthy != reportedStatus) {
+        if (isHealthy) {
+          unealthyPool.remove(this.instance.getId());
+          healthyPool.put(this.instance.getId(), this.instance);
+        } else {
+          unealthyPool.remove(this.instance.getId());
+          healthyPool.put(this.instance.getId(), this.instance);
+        }
+      }
+    }
+
+    @Override
+    public void run() {
+      logger.debug("EC2 monitor starting");
+      boolean run = true;
+      while(run) {
+        try{
+          Thread.sleep(SLEEP_TIME);
+          logger.debug("EC2 monitor running");
+          if (terminatingPool.containsKey(instance.getId())) {
+            if (instance.getInFlightRequests().size() <= 0) {
+              // TODO terminate instance
+            }
+            continue;
+          }
+          boolean currentStatus = null;
+          if (healthyPool.containsKey(instance.getId())) {
+            currentStatus = true;
+          } else if (unealthyPool.containsKey(instance.getId())) {
+            currentStatus = false;
+          } else if (flaggedPool.containsKey(instance.getId())) {
+            flaggedPool.remove(instance.getId());
+          }
+          healthCheck(currentStatus);
+        } catch (InterruptedException e) {
+          run = false;
+        }
+      }
+    }
+  }
 }
